092fe6db7ea26beef33e3e9963b5fe6348224fdd
ymk1t <yuma1102eff@gmail.com>
2019-07-14 00:45:43 +0900

    Updates

---
+++ precache-manifest.2abcc533282b231528edb03121efeff6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43fb3783e19db6219b5792db803bfddc",
+    "url": "/yosano/index.html"
+  },
+  {
+    "revision": "5657d46504dff99396f5",
+    "url": "/yosano/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/yosano/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "5657d46504dff99396f5",
+    "url": "/yosano/static/js/main.962c3729.chunk.js"
+  },
+  {
+    "revision": "c91b1690823660acea79",
+    "url": "/yosano/static/js/runtime~main.c600b025.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/yosano/static/media/logo.5d5d9eef.svg"
+  }
+]);
